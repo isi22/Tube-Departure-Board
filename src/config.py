381de00f4349269b4f08abb52e36c4e2ee3efa6e
Isabelle Kuder <isabelle.kuder@gmail.com>
2025-06-19 16:46:30 +0100

    rotated display

--- src/config.py
+++ src/config.py
@@ -1,6 +1,6 @@
 import os
 
-displayRotation = 0  # 0 is no rotation, 1 is rotate 90° clockwise, 2 is 180° rotation and 3 represents 270° rotation.
+displayRotation = 2  # 0 is no rotation, 1 is rotate 90° clockwise, 2 is 180° rotation and 3 represents 270° rotation.
 station = "Barons Court"  # The station to display on the board
 mode = "tube"
 
