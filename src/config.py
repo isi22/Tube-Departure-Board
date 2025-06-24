84d57115f78fe8a93a4f8df0d80b9b7acaccf417
Isabelle Kuder <isabelle.kuder@gmail.com>
2025-06-24 11:06:29 +0100

    readded piccadilly line

--- src/config.py
+++ src/config.py
@@ -4,13 +4,13 @@ displayRotation = 0  # 0 is no rotation, 1 is rotate 90° clockwise, 2 is 180° 
 station = "Barons Court"  # The station to display on the board
 mode = "tube"
 
-lines2 = [
+lines1 = [
     {"line": "Piccadilly", "direction": "eastbound"},
     {"line": "District", "direction": "eastbound"},
 ]  # The lines and directions to display on the board, use trial and error to find the correct direction for each line
 
-lines1 = [
-    # {"line": "Piccadilly", "direction": "westbound"},
+lines2 = [
+    {"line": "Piccadilly", "direction": "westbound"},
     {"line": "District", "direction": "westbound"},
 ]
 
